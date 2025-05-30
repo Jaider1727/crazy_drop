9f13e3e1ae422bd6765d3da5cd27fad8744622a4
Jaider1727 <jaidergonzalez019@gmail.com>
2025-05-30 15:43:08 -0500

    first commit

---
+++ src/main/java/gnt/plugin/crazy_drop/Crazy_drop.java
@@ -0,0 +1,17 @@
+package gnt.plugin.crazy_drop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Crazy_drop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
